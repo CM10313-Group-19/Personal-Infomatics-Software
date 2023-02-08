6c15252a84df47bd4d97b947e01049d7418937bd
ThomasCanning <thomas500book@gmail.com>
2023-02-08 09:49:36 +0000

    Adding example function to test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,17 @@ import javax.swing.*;
 
 public class Main {
 
+    //make a function that takes 2 integers and returns their highest common factor
+    public static int HCF(int a, int b){
+        int hcf = 1;
+        for(int i = 1; i <= a && i <= b; i++){
+            if(a % i == 0 && b % i == 0){
+                hcf = i;
+            }
+        }
+        return hcf;
+    }
+
     public static void main(String[] args){
 
     }
---
+++ src/test/java/HCFTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class HCFTest {
+
+    @org.junit.jupiter.api.Test
+    void HCF() {
+        assertEquals(2, Main.HCF(2, 6));
+        assertEquals(3, Main.HCF(2, 6));
+
+    }
+}
