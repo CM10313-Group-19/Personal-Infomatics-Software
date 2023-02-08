4d85da5a43b0501bd5ef3e0159a52bfeab740fb7
ThomasCanning <thomas500book@gmail.com>
2023-02-08 12:39:34 +0000

    Base template for code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,4 @@
-import javax.swing.*;
-
 public class Main {
-
-    //make a function that takes n integers and returns their highest common factor
-    public static int hcf(int... numbers){
-        int hcf = 1;
-        for(int i = 1; i <= numbers[0]; i++){
-            boolean isHcf = true;
-            for(int j = 0; j < numbers.length; j++){
-                if(numbers[j] % i != 0){
-                    isHcf = false;
-                    break;
-                }
-            }
-            if(isHcf){
-                hcf = i;
-            }
-        }
-        return hcf;
-    }
-
     public static void main(String[] args){
 
     }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,14 +1,6 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    @Test
-    void hcf() {
-        assertEquals(2, Main.hcf(2, 4, 6, 8));
-        assertEquals(1, Main.hcf(3, 5, 7, 9));
-        assertEquals(2, Main.hcf(2, 4, 6));
-        assertEquals(3, Main.hcf(3, 9, 15, 27));
-    }
 }
