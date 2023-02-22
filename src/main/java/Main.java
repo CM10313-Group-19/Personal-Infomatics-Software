6431122aa434df825660d41702d8d9d282162767
ThomasCanning <thomas500book@gmail.com>
2023-02-22 15:03:16 +0000

    Adding GUI for Login Page

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,13 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileNotFoundException;
 
-    //A function that takes n numbers and finds the highest common factor
-    public static int HCF(int... numbers){
-        int hcf = 1;
-        int min = numbers[0];
-        for(int i = 0; i < numbers.length; i++){
-            if(numbers[i] < min){
-                min = numbers[i];
-            }
-        }
-        for(int i = 1; i <= min; i++){
-            boolean isHCF = true;
-            for(int j = 0; j < numbers.length; j++){
-                if(numbers[j] % i != 0){
-                    isHCF = false;
-                    break;
-                }
-            }
-            if(isHCF){
-                hcf = i;
-            }
-        }
-        return hcf;
-    }
+public class Main {
 
     public static void main(String[] args){
 
-
+        //Creates and instance of the MainFrame class that contains all the other GUI panels are added
+        MainFrame frame = new MainFrame();
 
     }
 }
