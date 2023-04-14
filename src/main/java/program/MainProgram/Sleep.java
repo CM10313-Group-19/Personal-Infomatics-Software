e4c58ed7ef27f0ac12a1bac765188747f2be80c2
Alex <pinguoboy@gmail.com>
2023-04-14 22:18:43 +0100

    Added time validation and more towards sleep calculation

--- src/main/java/program/MainProgram/Sleep.java
+++ src/main/java/program/MainProgram/Sleep.java
@@ -4,6 +4,12 @@ import program.MainFrame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.ParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 
 //import java.time.*;
 
@@ -12,16 +18,53 @@ public class Sleep extends JPanel {
     private final JTextField sleepTimeTextField;
     private final JTextField wakeUpTextField;
 
-    static int calculateSleep(String timeString1, String timeString2){
+    private static String calculateSleep(String timeString1, String timeString2){
+        /** A bit broken atm cause cant work out time correctly **/
+        SimpleDateFormat format24HRS = new SimpleDateFormat("HH:mm");
         try {
-            int timeInteger1 = Integer.parseInt(timeString1);
-            int timeInteger2 = Integer.parseInt(timeString2);
+            Date timeDate1 = format24HRS.parse(timeString1);
+            Date timeDate2 = format24HRS.parse(timeString2);
+            System.out.println(timeDate1);
+            System.out.println(timeDate2);
 
-            return timeInteger2 - timeInteger1;
+            long calculateMS = Math.abs(timeDate2.getTime() - timeDate1.getTime()); // time in milliseconds for conversions
+            System.out.println(timeDate2.getTime());
+            System.out.println(calculateMS);
+
+            long calculateHours = (calculateMS / (60 * 60 * 1000)) % 24; // hours
+            System.out.println(calculateHours);
+
+            long calculateMins = (calculateMS / (60 * 1000)) % 60; // minutes
+            System.out.println(calculateMins);
+
+            return calculateHours + ":" + calculateMins;
         }
-        catch (NumberFormatException e) {
+        catch (ParseException e) {
+            return "00:00";
+        }
+    }
+
+    private static long calculateMinutes(String timeString1, String timeString2){
+        SimpleDateFormat format24HRS = new SimpleDateFormat("HH:mm");
+        try {
+            Date timeDate1 = format24HRS.parse(timeString1);
+            Date timeDate2 = format24HRS.parse(timeString2);
+
+            long calculateMS = Math.abs(timeDate2.getTime() - timeDate1.getTime()); // time in milliseconds for conversions
+            long calculateMins = (calculateMS / (60 * 1000)) % 60; // minutes
+
+            return calculateMins;
+        }
+        catch (ParseException e) {
             return 0;
         }
+    }
+
+
+    private boolean validateDateInput(String userTimeInput){
+        Pattern timeFormat = Pattern.compile("([01]?[0-9]|2[0-3]):[0-5][0-9]"); // regex pattern for 24hr time format
+        Matcher timeMatcher = timeFormat.matcher(userTimeInput);
+        return timeMatcher.matches();
     }
 
 
@@ -95,11 +138,21 @@ public class Sleep extends JPanel {
         this.add(mainPanel, BorderLayout.CENTER);
 
         submitButton.addActionListener(e -> {
-            String sleepTimeValue = sleepTimeTextField.getText();
+            String sleepTimeValue = sleepTimeTextField.getText(); // values of user input from text fields
             String wakeTimeValue = wakeUpTextField.getText();
-            System.out.println(sleepTimeValue);
-            System.out.println(wakeTimeValue);
-            JOptionPane.showMessageDialog(null, "Details Submitted.\nTime Slept: " + calculateSleep(sleepTimeValue, wakeTimeValue));
+
+            if (sleepTimeValue.equals("") || wakeTimeValue.equals("")){ // empty fields
+                JOptionPane.showMessageDialog(null, "Please fill in all fields");
+            }
+            else if (!(validateDateInput(sleepTimeValue)) || !(validateDateInput(wakeTimeValue))){ // wrong time format
+                JOptionPane.showMessageDialog(null, "Invalid time format. Please put time in the 24HR time format.\nFormat: HH:MM");
+            }
+            else{
+                System.out.println(sleepTimeValue);
+                System.out.println(wakeTimeValue);
+                JOptionPane.showMessageDialog(null, "Details Submitted.\nTime Slept: " + calculateSleep(sleepTimeValue, wakeTimeValue));
+            }
+
         });
 
         //If back button is clicked, loginPanel will be displayed
