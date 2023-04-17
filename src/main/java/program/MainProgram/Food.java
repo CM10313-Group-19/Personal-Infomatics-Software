93d76b4856794181bc2269e991d7d54d7c4acd9d
AlexTClarke <119004428+AlexTClarke@users.noreply.github.com>
2023-04-17 15:41:03 +0100

    Update to food and powerpoint design

--- src/main/java/program/MainProgram/Food.java
+++ src/main/java/program/MainProgram/Food.java
@@ -10,12 +10,11 @@ import java.util.regex.Pattern;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-
 //import java.time.*;
 
 public class Food extends Subpage {
 
-    //private final JTextField sleepTimeTextField;
+    // private final JTextField sleepTimeTextField;
     public Food(MainPage mainPage, MainFrame mainFrame) {
         super();
 
@@ -23,7 +22,22 @@ public class Food extends Subpage {
         exerciseLabel.setFont(new Font("Arial", Font.BOLD, 25));
         exerciseLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 30, 0));
 
-        //set layout to gridbaglayout and add components
+        // create and format 5ADLabel
+        JLabel fiveADLabel = new JLabel("5 a day");
+        fiveADLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        fiveADLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        fiveADLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+
+        // create and format buttons
+        JButton[] buttons = new JButton[5];
+        for (int i = 0; i < 5; i++) {
+            buttons[i] = new JButton();
+            buttons[i].setPreferredSize(new Dimension(50, 50));
+            buttons[i].setBackground(Color.WHITE);
+            buttons[i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        }
+
+        // set layout to gridbaglayout and add components
         mainPanel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.HORIZONTAL;
@@ -33,9 +47,18 @@ public class Food extends Subpage {
         c.gridy = 0;
         mainPanel.add(exerciseLabel, c);
 
+        c.gridy = 1;
+        mainPanel.add(fiveADLabel, c);
+
+        for (int i = 0; i < 5; i++) {
+            c.gridx = i;
+            c.gridy = 2;
+            mainPanel.add(buttons[i], c);
+        }
+
         this.add(mainPanel, BorderLayout.CENTER);
 
-        //If back button is clicked, loginPanel will be displayed
+        // If back button is clicked, loginPanel will be displayed
         backButton.addActionListener(e -> mainPage.SwitchPanel(mainPage.menuPanel));
     }
 }
